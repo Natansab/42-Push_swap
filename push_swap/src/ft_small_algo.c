57295069c1d771f273eeabd5e2a532a16b5b422b
Natansab <natansab@gmail.com>
2017-02-08 15:47:56 +0100

    norme ok

--- push_swap/src/ft_small_algo.c
+++ push_swap/src/ft_small_algo.c
@@ -6,16 +6,11 @@
 /*   By: nsabbah <nsabbah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 11:40:17 by nsabbah           #+#    #+#             */
-/*   Updated: 2017/02/08 15:02:18 by nsabbah          ###   ########.fr       */
+/*   Updated: 2017/02/08 15:45:57 by nsabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-typedef struct	s_min{
-	int one;
-	int two;
-}				t_min;
 
 int		ft_dist(t_list *start_a, int min)
 {
@@ -25,7 +20,7 @@ int		ft_dist(t_list *start_a, int min)
 	while (i <= nb_of_elem(start_a))
 	{
 		if (*(int*)start_a->content == min)
-			break;
+			break ;
 		start_a = start_a->next;
 		i++;
 	}
@@ -33,7 +28,8 @@ int		ft_dist(t_list *start_a, int min)
 	return (i);
 }
 
-void	move_min_to_b(t_list **start_a, t_list **start_b, int content, char *opsresult)
+void	move_min_to_b(t_list **start_a, t_list **start_b,
+			int content, char *opsresult)
 {
 	int distance;
 
@@ -62,6 +58,7 @@ t_min	ft_find_min(t_list *start_a)
 {
 	int		i;
 	t_min	min;
+
 	if (start_a && start_a->next)
 	{
 		min.one = *(int*)start_a->content;
@@ -85,37 +82,43 @@ t_min	ft_find_min(t_list *start_a)
 	return (min);
 }
 
+void	ft_small_algo_two(t_list *start_a, char *opsresult,
+			t_min min, t_list **start_b)
+{
+	if (ft_abs(ft_dist(start_a, min.one)) <=
+		ft_abs(ft_dist(start_a, min.two)) + 1)
+	{
+		move_min_to_b(&start_a, start_b, min.one, opsresult);
+		move_min_to_b(&start_a, start_b, min.two, opsresult);
+		order_in_place_a(&start_a, opsresult);
+		ft_push_elem(start_b, &start_a);
+		ft_push_elem(start_b, &start_a);
+		ft_strcat(opsresult, "pa\npa\n");
+	}
+	else
+	{
+		move_min_to_b(&start_a, start_b, min.two, opsresult);
+		move_min_to_b(&start_a, start_b, min.one, opsresult);
+		order_in_place_a(&start_a, opsresult);
+		ft_do_swap(start_b);
+		ft_push_elem(start_b, &start_a);
+		ft_push_elem(start_b, &start_a);
+		ft_strcat(opsresult, "sb\npa\npa\n");
+	}
+}
+
 void	ft_small_algo(t_list *start_a, char *opsresult)
 {
 	t_min	min;
-	t_list **start_b;
+	t_list	**start_b;
 
 	start_b = malloc(sizeof(t_list*));
-
 	if (nb_of_elem(start_a) <= 3)
 		order_in_place_a(&start_a, opsresult);
 	else
-		{
-			min = ft_find_min(start_a);
-			if (ft_abs(ft_dist(start_a, min.one)) <= ft_abs(ft_dist(start_a, min.two)) + 1)
-			{
-				move_min_to_b(&start_a, start_b, min.one, opsresult);
-				move_min_to_b(&start_a, start_b, min.two, opsresult);
-				order_in_place_a(&start_a, opsresult);
-				ft_push_elem(start_b, &start_a);
-				ft_push_elem(start_b, &start_a);
-				ft_strcat(opsresult, "pa\npa\n");
-			}
-			else
-			{
-				move_min_to_b(&start_a, start_b, min.two, opsresult);
-				move_min_to_b(&start_a, start_b, min.one, opsresult);
-				order_in_place_a(&start_a, opsresult);
-				ft_do_swap(start_b);
-				ft_push_elem(start_b, &start_a);
-				ft_push_elem(start_b, &start_a);
-				ft_strcat(opsresult, "sb\npa\npa\n");
-			}
-		}
+	{
+		min = ft_find_min(start_a);
+		ft_small_algo_two(start_a, opsresult, min, start_b);
+	}
 	free(start_b);
 }
